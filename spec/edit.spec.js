ea4ac309057b869141adb50d4c247d27dd4c7375
Barack Mukelenga <barackmukelenga100@gmail.com>
2021-07-13 18:55:26 +0200

    Add more unit tests

--- spec/edit.spec.js
+++ spec/edit.spec.js
@@ -13,20 +13,4 @@ describe('Calling edit todo ', () => {
   it('Throw an error of edit todo is called without argument', () => {
     expect(() => editTodo()).toThrow();
   });
-  it('should contain the text if the project is not given', () => {
-    const editTodo1 = (selectedProject) => {
-      const mainContainer = document.createElement('div');
-      const welcomeMsg = document.createElement('h2');
-      const inputsWrapper = document.createElement('div');
-      inputsWrapper.className = 'right-section-form-div';
-      if (!selectedProject) {
-        welcomeMsg.innerHTML = '';
-        inputsWrapper.innerHTML =
-          '<h3 class="edit_todo_title">Select first a project</h3>';
-        inputsWrapper.appendChild(welcomeMsg);
-        mainContainer.appendChild(inputsWrapper);
-      }
-      expect(inputsWrapper.value).toBe('Select first a project');
-    };
-  });
 });
